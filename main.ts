6c5304f712a9f88bb552765357130c7ae693b7ad
Blopvisje <122538086+Blopvisje@users.noreply.github.com>
2023-01-24 15:43:11 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,76 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        # # # # #
+        . . . . .
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        # # # # #
+        . . . . .
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # . # . #
+        # . # . #
+        # . # . #
+        # . # . #
+        # . # . #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # . # . #
+        # . # . #
+        # . # . #
+        # . # . #
+        # . # . #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
